90bec5c4f1e0a4dea911632a9aa9d9a65028242b
Gurjot Kaur Bandasha <gbandasha@dataseers.ai>
2020-07-27 23:46:24 -0400

    changes to test file system and call back functionalities

--- file_system.js
+++ file_system.js
@@ -1,19 +1,20 @@
-let fs = require("fs");
+var fs = require("fs");
 
-// // Asynchronous read
-// fs.readFile('input.txt', function (err, data) {
-//    if (err) {
-//       return console.error(err);
-//    }
-//    console.log("Asynchronous read: " + data.toString());
-// });
-//
-// // Synchronous read
+console.log("Program Started");
+// Asynchronous read
+fs.readFile('input.txt', function (err, data) {
+   if (err) {
+      return console.error(err);
+   }
+   console.log("Asynchronous read: " + data.toString());
+});
+
+// Synchronous read
 // var data = fs.readFileSync('input.txt');
 // console.log("Synchronous read: " + data.toString());
-//
+
 // console.log("Program Ended");
-//
+
 //
 // // Asynchronous - Opening File
 // console.log("Going to open file!");
@@ -26,15 +27,15 @@ let fs = require("fs");
 //
 //
 //
-console.log("Going to get file info!");
-fs.stat('input.txt', function (err, stats) {
-   if (err) {
-      return console.error(err);
-   }
-   console.log(stats);
-   console.log("Got file info successfully!");
+// console.log("Going to get file info!");
+// fs.stat('input.txt', function (err, stats) {
+//    if (err) {
+//       return console.error(err);
+//    }
+//    console.log(stats);
+//    console.log("Got file info successfully!");
 
-   // Check file type
-   console.log("isFile ? " + stats.isFile());
-   console.log("isDirectory ? " + stats.isDirectory());
-});
+//    // Check file type
+//    console.log("isFile ? " + stats.isFile());
+//    console.log("isDirectory ? " + stats.isDirectory());
+// });
